0a8cf0be01c28474be00f87d9d26b69d94764539
Nathalie_Brown <nbrown92@charlotte.edu>
2025-11-21 18:57:06 -0500

    Updated ingredient routes

--- src/routes/ingredientRoutes.js
+++ src/routes/ingredientRoutes.js
@@ -4,9 +4,35 @@ const { requireAuth, requireRole } = require("../middleware/auth");
 const ingredientController = require("../controllers/ingredientController");
 
 router.get("/", ingredientController.searchIngredients);
+
+
 router.get("/:id", ingredientController.getIngredient);
-router.post("/", requireAuth, ingredientController.createIngredient);      // you can add role checks here if needed
-router.put("/:id", requireAuth, requireRole("ADMIN"), ingredientController.updateIngredient);
-router.delete("/:id", requireAuth, requireRole("ADMIN"), ingredientController.deleteIngredient);
+
+
+router.post(
+  "/",
+  requireAuth,
+  (req, res, next) => {
+    if (req.user.role === "CREATOR" || req.user.role === "ADMIN") return next();
+    return res.status(403).json({ error: "Forbidden" });
+  },
+  ingredientController.createIngredient
+);
+
+
+router.put(
+  "/:id",
+  requireAuth,
+  requireRole("ADMIN"),
+  ingredientController.updateIngredient
+);
+
+
+router.delete(
+  "/:id",
+  requireAuth,
+  requireRole("ADMIN"),
+  ingredientController.deleteIngredient
+);
 
 module.exports = router;
