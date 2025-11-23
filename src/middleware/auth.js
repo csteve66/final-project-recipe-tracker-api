70fe50c6da9235e00ed7be6360a52b6513a504ca
jasminebiggs <jbiggs7@charlotte.edu>
2025-11-22 20:56:16 -0500

    docs: update OpenAPI spec to align with new middleware and validators

--- src/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-export function requireAuth(req, res, next) {
-  // TEMP for development/testing only
-  req.user = {
-    userid: 1,      // pretend logged-in user with id 1
-    role: 'ADMIN',  // or 'USER' if you want to test restrictions
-  };
-  next();
-}
